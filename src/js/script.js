953275153ab33323f6dacd01ac5d00c2c9ea9221
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-15 15:26:00 +0200

    hw_2 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+//task_1//
+
+let admin;
+let name = 'Jonh';
+admin = name;
+console.log(admin);
+console.log(name);
+
+//task_2//
+
+let ourPlanetName;
+let currentUserName;
+
+//task_3//
+
+let num;
+num = '10' + 10;
+console.log(num);
+
