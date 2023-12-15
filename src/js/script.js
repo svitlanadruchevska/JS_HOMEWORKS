299bce5b669e51fa2e3ae645889b84792f5d218a
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-15 16:59:51 +0200

    hw_3 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+const num1 = prompt('Enter first number'), num2 = prompt('Enter second number');
+
+console.log('Result : ' +num1 + ' + ' + num2 +' = ' +(+num1 + +num2));
+console.log('Result : ' +num1 + ' - ' + num2 +' = ' +(+num1 - +num2));
+console.log('Result : ' +num1 + ' * ' + num2 +' = ' +(+num1 * +num2));
+console.log('Result : ' +num1 + ' / ' + num2 +' = ' +(+num1 / +num2));
