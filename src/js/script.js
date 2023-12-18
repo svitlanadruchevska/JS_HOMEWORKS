875b67749da02bbccc9b0aa46202a0c992f3cf3d
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-18 21:59:21 +0200

    hw_4 done

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,17 @@
 
 //task_1
 
-const firstText = 'Please, enter your text...';
-const secondText = 'and some more...';
-const thirdText = "and just a little more. Thank's a lot!";
+const firstText = prompt('Please, enter your text...');
+const secondText = prompt('and some more...');
+const thirdText = prompt("and just a little more. Thank's a lot!");
 
-console.log(firstText + secondText +thirdText);
+alert(firstText + secondText +thirdText);
 
 //task_2
 
 const number = 78832;
-console.log(number);
 const numberStr = String(number);
+
 console.log(numberStr[0] + ' ' + numberStr[1] + ' ' + numberStr[2] + ' ' + numberStr[3] + ' ' + numberStr[4]);
 
 
