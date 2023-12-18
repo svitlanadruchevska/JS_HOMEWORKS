32329d973a73bc48518f71d14db046a32f236a5f
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-18 21:42:11 +0200

    hw_4 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+//task_1
+
+const firstText = 'Please, enter your text...';
+const secondText = 'and some more...';
+const thirdText = "and just a little more. Thank's a lot!";
+
+console.log(firstText + secondText +thirdText);
+
+//task_2
+
+const number = 78832;
+console.log(number);
+const numberStr = String(number);
+console.log(numberStr[0] + ' ' + numberStr[1] + ' ' + numberStr[2] + ' ' + numberStr[3] + ' ' + numberStr[4]);
+
+
+
+
+
