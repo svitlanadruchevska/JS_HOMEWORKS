bdd74f16229149515b0cab30edbfcc85d32d46b8
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-25 13:48:28 +0200

    hw_6 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,116 @@
 'use strict';
+
+//task1
+
+console.log('task1');
+
+const student =  {
+    name: 'Ann',
+    age: 25,
+    grade: 95,
+};
+
+console.log(student);
+
+student.grade = 100;
+
+console.log(student);
+
+student.course = 'second';
+
+console.log(student);
+
+//task2
+
+console.log('task2');
+
+ const book = {
+     title: 'The Old Man and the Sea',
+     author: 'Ernest Hemingway',
+     year: 1952,
+     publisher: {
+         name: 'Charles Scribner\'s Sons',
+         location: 'Bimini (Bahamas)',
+     }
+};
+
+ console.log(book);
+
+//task3
+
+console.log('task3');
+
+const movie = {
+    title: 'Wonka',
+    director: 'Paul King',
+    year: 2023,
+    genre: 'musical fantasy film',
+};
+
+for (const prop in movie) {
+    console.log('movie ' + prop + ' = ' + movie[prop])
+}
+
+//task4
+
+console.log('task4');
+
+const person1 = {
+    name: 'Mary',
+    age: 32,
+};
+
+const person2 = {
+    name: 'Mary',
+    age: 32,
+};
+
+console.log(person1===person2);
+
+function compare (person1, person2){
+        if (person1.length === person2.length) {
+        return true;
+        }
+        else{
+            if (person1.length !== person2.length) {
+                return false;
+            }
+        }
+}
+
+console.log(compare(person1, person2));
+
+//task5
+
+console.log('task5');
+
+const arrAnimals = [
+    {
+        name: 'Fox',
+        type: 'Mammalia',
+    },
+    {
+        name: 'Frog',
+        type: 'Amphibia',
+    },
+    {
+        name: 'Crocodile',
+        type: 'Reptilia',
+    },
+    {
+        name: 'Parrot',
+        type: 'Birds',
+    },
+];
+
+console.log(arrAnimals);
+
+arrAnimals[4] = {
+    name: 'Lemur',
+    type: 'Mammalia',
+};
+
+console.log(arrAnimals);
+
+
+
