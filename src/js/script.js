58f77bc221f30cc38ab352c0764dd0247262c768
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-29 10:24:35 +0200

    hw_13 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const num1 = prompt('Enter first number');
+const num2 = prompt('Enter second number');
+const operator = prompt('Select one of the operations( + , - , * , /)');
+
+switch (operator) {
+    case '+' :
+        alert(+num1 + ' + ' + num2 +' = ' +(+num1 + +num2));
+        break;
+    case '-' :
+        alert(+num1 + ' - ' + num2 +' = ' +(num1 - num2));
+        break;
+    case '*' :
+        alert(+num1 + ' * ' + num2 +' = ' +(num1 * num2));
+        break;
+    case '/' :
+        alert(+num1 + ' / ' + num2 +' = ' +(num1 / num2).toFixed(2));
+        break;
+    default:
+        alert("No such operator exists");
+}
+
