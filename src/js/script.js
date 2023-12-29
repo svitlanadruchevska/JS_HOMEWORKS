d529912b67e13fe6fabaf050050c65cf4661ade1
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-29 11:10:49 +0200

    hw_15 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const num1 = +prompt('Enter first number');
+const num2 = +prompt('Enter second number');
+const num3 = +prompt('Enter third number');
+const numArr = [num1, num2, num3];
+
+alert( 'Arithmetic mean of numbers ' + num1 + ', ' + num2 + ', ' +num3 + ' = ' +
+    +((num1+num2+num3)/numArr.length).toFixed(2));
+
