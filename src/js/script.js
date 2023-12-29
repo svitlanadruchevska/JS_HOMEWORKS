5779b0916c8801e5c76815aa73c156a928ecaa72
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-29 10:41:12 +0200

    hw_13 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const userHours = prompt('Choose any number of hours :');
+
+const userSeconds = (userHours*3600 +' sec');
+
+alert('In ' + userHours + ' hours' + ' ' + userSeconds);
