d184695e83c0996386c4a43d49424f43fef0fe4a
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2023-12-29 09:51:38 +0200

    hw_12 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const num1 = prompt('Enter first number');
+const num2 = prompt('Enter second number');
+
+alert('Addition : ' +num1 + ' + ' + num2 +' = ' +(+num1 + +num2));
+alert('Subtraction : ' +num1 + ' - ' + num2 +' = ' +(num1 - num2));
+alert('Multiplication : ' +num1 + ' * ' + num2 +' = ' +(num1 * num2));
+alert('Division : ' +num1 + ' / ' + num2 +' = ' +(num1 / num2));
