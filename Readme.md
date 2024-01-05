5634a52b6c8adea84918f49193a0179c92b497cb
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-05 19:34:36 +0200

    hw_15 done

--- Readme.md
+++ Readme.md
@@ -1,1 +1,14 @@
-_**Homework description**_
+`Переписати код нижче з використанням конструкції switch…case`
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+if(numOrStr === null) {
+console.log('ви скасували')
+} else if( numOrStr.trim() === '' ) {
+console.log('Empty String');
+} else if ( isNaN( +numOrStr ) ) {
+console.log(' number is Ba_NaN')
+} else {
+console.log('OK!')
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const numOrStr = prompt('Input number or string');
+
+console.log(numOrStr);
+
+switch (true) {
+    case (numOrStr === null):
+        console.log('ви скасували');
+        break;
+    case (numOrStr.trim() === ''):
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+}
+
