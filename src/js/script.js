5a71f94bce34083f43eeb22b619eb34eddf4a524
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-06 20:22:01 +0200

    hw_20 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,3 @@
-/*
-'use strict';
-
-const userNum = prompt('Введіть число');
-
-if (!isNaN(userNum)) {
-    let wordCorrect;
-    if (+userNum === 1) {
-        wordCorrect = 'рік';
-    } else if (((userNum > 20) % 10) === 1) {
-        wordCorrect = 'рік';
-    } else if ((userNum  % 10) >= 2 && (userNum % 10) <= 4) {
-        wordCorrect = 'роки';
-    } else {
-        wordCorrect = 'років';
-    }
-
-    alert(userNum + ' ' + wordCorrect);
-} else {
-
-    alert('Будь ласка, введіть число.');
-}*/
-
 'use strict';
 
 const userNum = prompt('Введіть число');
