fa05e231e398e8d34b18aa47ddd319840f383b37
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-06 17:21:26 +0200

    hw_16

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,49 @@
 'use strict';
+
+const userYearOfBirth = prompt( 'В якому році ти народився?');
+const userCityResidence = prompt( 'В якому місті ти живеш?');
+const userKindSport = prompt( 'Який твій улюблений вид спорту?');
+
+if (userYearOfBirth === null || userCityResidence === null || userKindSport === null) {
+    alert("Шкода, що ти не захотів ввести свій(ю): " + (userYearOfBirth === null ? "рік народження " : "") +
+        (userCityResidence === null ? (userYearOfBirth === null ? "" : ", ") + "місто проживання" : "") +
+        (userKindSport === null ? ((userYearOfBirth === null || userCityResidence === null) ? ", " : "") +
+            "улюблений вид спорту" : "") + ".");
+} else {
+    const userAge = 2024 - parseInt(userYearOfBirth);
+
+    let message = "Твій вік: " + userAge + " р.\n";
+
+    if (userCityResidence === "Київ" || userCityResidence === "Вашингтон" || userCityResidence === "Лондон") {
+        message += "Ти живеш у столиці " + getCountry(userCityResidence) + ".\n";
+    } else {
+        message += "Ти живеш у місті " + userCityResidence + ".\n";
+    }
+
+    const champions = {
+        "футбол": "Ліонель Мессі",
+        "формула-1": "Міхаель Шумахер",
+        "баскетбол": "Майкл Джордан"
+    };
+
+    if (champions.hasOwnProperty(userKindSport)) {
+        message += "Круто! Хочеш стати як " + champions[userKindSport] + "?";
+    } else {
+        message += "Твій улюблений вид спорту: " + userKindSport + ".";
+    }
+
+    alert(message);
+}
+
+function getCountry(capital) {
+    switch (capital) {
+        case "Київ":
+            return "України";
+        case "Вашингтон":
+            return "США";
+        case "Лондон":
+            return "Великої Британії";
+    }
+}
+
+
