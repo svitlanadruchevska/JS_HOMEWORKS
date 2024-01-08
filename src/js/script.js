f337ee71a4505c239f6548640a50fbe742020e5d
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-08 20:09:34 +0200

    hw_24 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const userNum = prompt('Введіть будь-яке число');
+
+if (userNum < 1) {
+    console.log(NaN);
+} else {
+    let divisor = 2;
+    while (divisor <= userNum) {
+        if (userNum % divisor === 0) {
+            console.log('Найменший дільник числа ' + userNum + ' це ' + divisor);
+            break;
+        }
+        divisor++;
+    }
+
+    if (divisor / userNum === 1) {
+        console.log(userNum + ' це просте число');
+    }
+}
+
+
