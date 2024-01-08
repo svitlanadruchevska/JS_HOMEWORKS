3fdf553ef9f2b562097df2e25a7264e6fbb5066b
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-08 20:32:48 +0200

    hw_23 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//part1
+
+const arr = [1, 2, 3, 4, 5];
+
+let sumArr = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sumArr += arr[i];
+}
+console.log('Сума елементів масиву = ' + sumArr);
+
+//part2
+
+let sumSquares = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sumSquares += Math.pow(arr[i], 2);
+}
+
+console.log('Сума квадратів елементів масиву = ' + sumSquares);
+
