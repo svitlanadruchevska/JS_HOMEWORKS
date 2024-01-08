c819c60e325ecd587ce19942766edbf0a1cf2d01
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-08 20:57:27 +0200

    hw_25 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let sum = 0;
+
+outerLoop:
+    for (let i = 1; i <= 20; i++) {
+        if (i % 2 === 0) {
+            continue outerLoop;
+        }
+        sum += i;
+    }
+console.log("Сума непарних чисел в діапазоні від 1 до 20: " + sum);
