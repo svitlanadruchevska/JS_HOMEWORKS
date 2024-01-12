b176d1879c50218c06d5ed03841719f75810963b
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-12 12:45:31 +0200

    hw_26 done

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ function generateKey(length, characters) {
     let result = '';
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charactersLength);
+        const randomIndex = Math.random() * charactersLength;
         result += characters[randomIndex];
     }
     return result;
