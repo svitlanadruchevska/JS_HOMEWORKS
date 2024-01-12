4aae373d287c8261f98d7b09ef004dd6784f28e4
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-12 17:27:44 +0200

    hw_26 update

--- src/js/script.js
+++ src/js/script.js
@@ -7,11 +7,11 @@ function padString(str, length, symb = '', addSymb = false) {
     if (length <= str.length) {
         return str.substring(0, length);
     }
-    const padSymb = length - str.length;
-    const padd = symb.substring(padSymb);
-    return  addSymb ? padd + str : str + padd;
+    const padSymb = symb.repeat(length - str.length);
+    return  addSymb ? padSymb + str : str + padSymb;
 }
 
-console.log(padString('привіт', 3));
-console.log(padString('привіт', 8, ''));
+console.log(padString('привіт', 2));
+console.log(padString('привіт', 8));
 console.log(padString('привіт', 6, '*', false));
+console.log(padString('привіт', 10, '-', true));
