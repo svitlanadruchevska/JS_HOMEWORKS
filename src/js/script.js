e281c7921a976a8ed4aa0c5b07b8eb98b26f0e93
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-12 12:06:38 +0200

    hw_29 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+function checkout(arr) {
+    const exampleArr = [];
+    if (arr.length === 0) {
+        return 'Масив порожній';
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            exampleArr.push(arr[i]);
+        }
+    }
+    return exampleArr.length > 0 ? exampleArr : null;
+}
+
+console.log(checkout(arr));
