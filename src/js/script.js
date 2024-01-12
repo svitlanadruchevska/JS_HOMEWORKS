f43c8cd9c76c696e0190959278e1766abed7b745
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-12 11:54:34 +0200

    hw_28 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function padString(str, length, symb = '', addSymb = false) {
+    if (typeof str !== 'string' || typeof length !== 'number' || typeof symb !== 'string' || typeof addSymb !== 'boolean') {
+        return 'Помилка: невірні типи аргументів';
+    }
+    if (length <= str.length) {
+        return str.substring(0, length);
+    }
+    const padSymb = length - str.length;
+    const padd = symb.substring(padSymb);
+    return  addSymb ? padd + str : str + padd;
+}
+
+console.log(padString('привіт', 3));
+console.log(padString('привіт', 8, ''));
+console.log(padString('привіт', 6, '*', false));
