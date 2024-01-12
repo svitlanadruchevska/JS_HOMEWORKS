5298daf19f0da0b04588067cd3e2ec2f663762c0
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-12 17:34:55 +0200

    hw_30 update

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 
 function potatosForBorsch(potatoWeigth, potatoQuantity, potatoPrice, borschQuantity) {
     let potatoCost = (potatoWeigth * potatoQuantity) / 1000 * potatoPrice;
-    return Math.ceil(potatoCost * borschQuantity);
+    return Math.round(potatoCost * borschQuantity);
 }
 
 console.log('Вартість картоплі для приготування 48л борщу : ' + potatosForBorsch(75, 4, 13, 48) + 'грн');
