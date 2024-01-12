ce0a5245686d2644f9a0b47156425d77f34bf06a
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-12 10:45:17 +0200

    hw_27 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const userNum = parseInt(prompt('Будь ласка, введіть будь-яке число'));
+const degree = parseInt(prompt('Введіть ступінь, до якої необхідно піднести число'));
+function exponentiation(userNum, degree) {
+    if (userNum === null || isNaN(userNum) || degree === null || isNaN(degree)) {
+        return 'Помилка. Будь ласка, введіть корректне число';
+    }
+    return  Math.pow(userNum, degree);
+}
+
+let result = exponentiation(userNum, degree);
+alert('Результат приведення числа ' + userNum + ' в ' + degree + ' ступінь : ' + result);
+
+
