e65d8ea7537fb4f770ef09244c2e7399142c965d
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-12 17:43:39 +0200

    hw_26 update

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,8 @@
 'use strict';
-
 function generateKey(length, characters) {
     let result = '';
-    const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.random() * charactersLength;
+        const randomIndex = Math.floor(Math.random() * characters.length);
         result += characters[randomIndex];
     }
     return result;
