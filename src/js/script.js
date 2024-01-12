9172d205dbd31085ed73730b87a2ef704460b164
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-12 09:54:10 +0200

    hw_26 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function generateKey(length, characters) {
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+        result += characters[randomIndex];
+    }
+    return result;
+}
+
+const randomString = prompt('Введіть будь-які символи');
+const key = generateKey(10, randomString);
+console.log(key);
+
