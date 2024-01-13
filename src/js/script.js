64c3b7a2eeae963904d9d1b9a128e6b375ac2a6e
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-13 21:28:18 +0200

    hw_29 update

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,9 @@ const arr = [1, 2, 3, -1, -2, -3];
 
 function checkout(arr) {
     const exampleArr = [];
-    if (arr.length === 0) {
-        return 'Масив порожній';
-    }
+    if (arr.length === 0) return 'Масив порожній';
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 0) {
-            exampleArr.push(arr[i]);
-        }
+        if (arr[i] <= 0) exampleArr.push(arr[i]);
     }
     return exampleArr.length > 0 ? exampleArr : null;
 }
