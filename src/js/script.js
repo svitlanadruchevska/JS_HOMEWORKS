0a1c279ba26dfabc0e3b24155042c17d81717c7d
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-13 20:44:19 +0200

    hw_26 update_const

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function potatosForBorsch(potatoWeigth, potatoQuantity, potatoPrice, borschQuantity) {
-    let potatoCost = (potatoWeigth * potatoQuantity) / 1000 * potatoPrice;
+    const potatoCost = (potatoWeigth * potatoQuantity) / 1000 * potatoPrice;
     return Math.round(potatoCost * borschQuantity);
 }
 
