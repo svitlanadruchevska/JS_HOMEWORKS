f12c23286ab7f54c47d6b05c9156f69ea4e68b62
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-15 19:17:09 +0200

    hw_31 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const randomNum = function (num = 100) {
+    let usedNumbers = [];
+    const selection = function () {
+        if (usedNumbers.length === num) {
+            usedNumbers = [];
+        }
+        const result = Math.floor(Math.random() * num) + 1;
+        if (usedNumbers.includes(result)) {
+            return selection();
+        } else {
+            usedNumbers.push(result);
+            return result;
+        }
+    }
+    return selection();
+}
+console.log(randomNum());
