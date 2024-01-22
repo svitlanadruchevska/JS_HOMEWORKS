5b06269682419f4965f38b4185cea34f0bf38cfe
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-22 19:29:59 +0200

    hw_34 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-'use strict';
+'use strict'
+
+const createUser = function (name, age) {
+  const user = {
+    name,
+    age
+  }
+  user.greet = greet
+  return user
+}
+
+const greet = function () {
+  console.log(`Привіт, мене звати ${this.name}!`)
+}
+
+const user1 = createUser('Марія', 25)
+const user2 = createUser('Роман', 30)
+
+user1.greet()
+user2.greet()
