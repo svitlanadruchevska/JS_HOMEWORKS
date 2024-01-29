4db54cd1fe237056a98433f511167a9c249b1ad2
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-29 18:46:59 +0200

    hw_38 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const updateQueryString = function (url, key, value) {
+  const parsedUrl = new URL(url);
+  parsedUrl.searchParams.set(key, value);
+
+  return parsedUrl.toString();
+}
+
+console.log(updateQueryString('https://example.com/path?query=123', 'query', '456'));
+console.log(updateQueryString('https://example.com/path?query=123', 'newParam', '772'));
