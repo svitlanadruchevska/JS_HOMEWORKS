0fecfd865408422d9a214374f5b8e1bd82b820a9
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-29 18:28:38 +0200

    hw_37 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const parseUrl = function (url) {
+  const parsedUrl = new URL(url);
+
+  return {
+    href: parsedUrl.href,
+    protocol: parsedUrl.protocol,
+    host: parsedUrl.host,
+    hostname: parsedUrl.hostname,
+    pathname: parsedUrl.pathname,
+    search: parsedUrl.search,
+    hash: parsedUrl.hash,
+    origin: parsedUrl.origin
+  };
+}
+const url = 'https://example.com:8080/path/name?query=123#hash';
+const parsedUrl = parseUrl(url);
+console.log(parsedUrl);
