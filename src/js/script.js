7c80853f003e3f06473a2aacee982efd91723ace
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-01-29 18:07:59 +0200

    hw_36 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+/* в цьому випадку хойстинг призведе до успішного виконання коду */
+const multiply = function (a, b) {
+  return a * b;
+};
+console.log(multiply(2, 4));
+
+/* в цьому випадку хойстинг призведе до помилки, тому що змінна не може бути використана до того як вона визначена */
+console.log(sum(2, 4));
+const sum = function (a, b) {
+  return a + b;
+};
