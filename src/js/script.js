48112468c6921e544d90850ebe8db3156621a1cc
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-11 20:56:30 +0200

    hw_31 update_3

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,16 @@
 'use strict';
 const randomNumGenerate = function (num = 100) {
-    const usedNumbers = [];
-    const selection = function () {
-        const result = Math.floor(Math.random() * num) + 1;
-        if (usedNumbers.includes(result)) {
-            return selection();
-        } else {
-            usedNumbers.push(result);
-            return result;
-        }
+  const usedNumbers = [];
+  const selection = function () {
+    const result = Math.floor(Math.random() * num) + 1;
+    if (usedNumbers.includes(result)) {
+      return selection();
     }
-    return selection;
-}
+    usedNumbers.push(result);
+    return result;
+  };
+  return selection;
+};
 
 const randomNum = randomNumGenerate();
 
