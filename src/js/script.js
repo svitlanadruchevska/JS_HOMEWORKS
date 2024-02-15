4b5c104388ebf94bc838bf7282abe2ab6e81e147
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-15 17:59:12 +0200

    hw_44 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const newArr = function (arr) {
+  if (arguments.length > 1) {
+    throw new Error('Function accepts only 1 argument, too much arguments provided');
+  }
+  const result = [];
+
+  function flatArr (array) {
+    for (const num of array) {
+      if (Array.isArray(num)) {
+        flatArr(num);
+      } else {
+        result.push(num);
+      }
+    }
+  }
+
+  flatArr(arr);
+
+  return result;
+};
+
+const complexArray = [
+  [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+  [5, [6, [7, [8, 9]]]],
+  [[10, 11], 12, 13]
+];
+
+const flatArray = newArr(complexArray);
+console.log(flatArray);
