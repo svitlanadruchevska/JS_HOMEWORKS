ed5a05242de27d77debd900a9c12b388493a215f
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-15 18:23:02 +0200

    hw_45 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const ladder = {
+  step: 0,
+  up: function () {
+    this.step++;
+    return this;
+  },
+  down: function () {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    alert(this.step);
+    return this;
+  }
+};
+
+ladder.up().up().down().showStep();
