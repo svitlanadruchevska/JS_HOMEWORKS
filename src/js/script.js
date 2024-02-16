17850df5c4a5fe06de38870623c8688356b1f36f
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-16 13:36:59 +0200

    hw_46 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const cats = [
+  {
+    name: 'Marta',
+    age: '10'
+  },
+  {
+    name: 'Foxy',
+    age: '5'
+  },
+  {
+    name: 'Leo',
+    age: '2'
+  }
+];
+
+const dogs = [
+  {
+    name: 'Jack',
+    age: '1'
+  },
+  {
+    name: 'Patron',
+    age: '6'
+  },
+  {
+    name: 'Fill',
+    age: '12'
+  }
+];
+
+const pets = function () {
+  return this.name + ' - ' + this.age + ' yr';
+};
+
+cats.forEach(function (cat) {
+  console.log(pets.apply(cat));
+});
+
+dogs.forEach(function (dog) {
+  console.log(pets.apply(dog));
+});
