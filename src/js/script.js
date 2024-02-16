68cdce674f2af82bdacf6713b534a3f8076f5177
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-16 13:49:52 +0200

    hw_47 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const getFlatNumber = function (floor, building) {
+  return 'Flat number ' + this.number + ' is on floor ' + floor + ' of building ' + building + '.';
+};
+
+const room = {
+  number: 101
+};
+
+const getFlat101 = getFlatNumber.bind(room);
+
+console.log(getFlat101(7, '1-A'));
