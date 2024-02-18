96231b9999669e866eca8557612fc83e1e454467
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-18 16:45:20 +0200

    hw_47 update

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,19 @@
 'use strict';
 
+const myBind = function (func, context) {
+  return function (...flats) {
+    return func.apply(context, flats);
+  };
+};
+
 const getFlatNumber = function (floor, building) {
   return 'Flat number ' + this.number + ' is on floor ' + floor + ' of building ' + building + '.';
 };
 
-const room = {
+const flat = {
   number: 101
 };
 
-const getFlat101 = getFlatNumber.bind(room);
+const getFlat101 = myBind(getFlatNumber, flat);
 
 console.log(getFlat101(7, '1-A'));
