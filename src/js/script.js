e099a20c8cfa64cd776a82b7de90e60b9b3786db
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-19 18:10:49 +0200

    hw_50 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const products = {
+  item1: {
+    name: 'Картопля',
+    price: 100
+  },
+  item2: {
+    name: 'Помідор',
+    price: 200
+  },
+  item3: {
+    name: 'Огірок',
+    price: 300
+  }
+};
+const discount = (products) => {
+  return Object.keys(products).reduce((acc, key) => {
+    acc[key] = {
+      ...products[key],
+      price: products[key].price * 0.9
+    };
+    return acc;
+  }, {});
+};
+
+console.log(discount(products));
