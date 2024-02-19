a26e94b6c7d225afa8d81c4531a7ee4ee08cdf9c
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-19 17:01:28 +0200

    hw_48 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const sum = (num1, num2) => num1 + num2;
+console.log(sum(20, 12));
+
+const someStr = str => str.length;
+console.log(someStr('Hello'));
+
+const newArr = arr => arr.map(num => num + 1);
+console.log(newArr([1, 2, 4, 8]));
