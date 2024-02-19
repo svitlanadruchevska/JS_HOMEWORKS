886de65cea7d19702aed7a1b44841f190d054ab0
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-02-19 18:30:52 +0200

    hw_51 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const parseJSON = function (jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (error) {
+    return 'Error! Invalid JSON string';
+  }
+};
+
+// Приклад використання:
+console.log(parseJSON('{"name": "John", "age": 30}'));
+console.log(parseJSON('{"name": "John", "age":'));
