0d8b1fcd98ec25a04acc0aed3dd96fe75b88e889
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-03-04 08:46:46 +0200

    hw_55 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+const createTable = function () {
+  const table = document.createElement('table');
+  let number = 1;
+  for (let i = 0; i < 10; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement('td');
+      cell.textContent = number.toString();
+      number++;
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+  table.classList.add('bordered-table');
+
+  document.getElementById('table-container').appendChild(table);
+};
+
+createTable();
