f145e740749586f9ac40ca107b31786ab077eb09
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-03-04 08:28:52 +0200

    hw_54 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const generateList = function generateList (array) {
+  const ul = document.createElement('ul');
+  array.forEach(item => {
+    const li = document.createElement('li');
+    if (Array.isArray(item)) {
+      li.appendChild(generateList(item));
+    } else {
+      li.textContent = item;
+    }
+    ul.appendChild(li);
+  });
+  return ul;
+};
+
+const array = [1, 2, [1.1, 1.2, 1.3], 3, 4, 5];
+const list = generateList(array);
+document.body.appendChild(list);
