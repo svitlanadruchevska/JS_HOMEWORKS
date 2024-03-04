4171518b20b3fc7c883c2598bef0478d423971a0
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-03-04 09:04:56 +0200

    hw_58 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+const checkNumber = function () {
+  const userInput = document.getElementById('numberInput').value;
+  if (!userInput || isNaN(userInput)) {
+    alert('Введіть дійсне число!');
+    return;
+  }
+  const number = Number(userInput);
+  if (number % 2 === 0) {
+    alert('Парне число');
+  } else {
+    alert('Непарне число');
+  }
+};
