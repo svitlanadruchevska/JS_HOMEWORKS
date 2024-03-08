60a003bc8378fc120329030d3475563781c41b65
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-03-08 17:02:10 +0200

    hw_59 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-'use strict';
+(function () {
+  const saveText = function () {
+    const inputField = document.getElementById('inputText');
+    const text = inputField.value;
+    if (text.trim() !== '') {
+      const newElement = document.createElement('p');
+      newElement.innerHTML = text;
+      const outputDiv = document.getElementById('output');
+      outputDiv.prepend(newElement);
+      inputField.value = '';
+    } else {
+      alert('Будь ласка, введіть текст!');
+    }
+  };
+  document.getElementById('saveButton').addEventListener('click', saveText);
+})();
