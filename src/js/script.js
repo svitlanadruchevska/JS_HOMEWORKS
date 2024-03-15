13a17826c19f31b7765406b7e1232606bfeafff4
svitlanadruchevska <129722125+svitlanadruchevska@users.noreply.github.com>
2024-03-15 08:33:48 +0200

    hw_60 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-'use strict';
+(function () {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if (name === '' || email === '' || password === '') {
+      alert('Будь ласка, заповніть усі поля');
+    } else {
+      alert('Реєстрація успішна');
+    }
+  });
+})();
